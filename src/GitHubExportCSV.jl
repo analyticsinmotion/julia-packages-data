6e5c727817c6571df2a086c6016aec54814a012c
Ross Armstrong <52817125+rossarmstrong@users.noreply.github.com>
2023-10-17 13:39:39 +1100

    Initial commit

--- src/GitHubExportCSV.jl
+++ src/GitHubExportCSV.jl
@@ -31,61 +31,47 @@ export_csv("output", df, "data/")  # Saves the DataFrame to "data/output.csv"
 ```
 
 """
-function export_csv(output_file_name::String, input_data_source::DataFrame, output_file_extension::String = ".csv")
-    
-    # Build the file path
-    #full_path = string(output_file_path, output_file_name, output_file_extension)
+function export_csv(data::String, repo_owner::String, repo_name::String, branch_name::String, file_path::String, access_token::String)
 
-    # Construct the path to the CSV file using the relative path
-    #full_file_name = string(output_file_name, output_file_extension)
-    #csv_path = joinpath(dirname(@__FILE__), "..", "data", full_file_name)
-    #println("File path in Github: ", csv_path)
-    
-    # Construct the path to the CSV file
-    full_file_name = string(output_file_name, output_file_extension)
-    repo_dir = ENV["GITHUB_WORKSPACE"]
-    println("Repo Directory: ", repo_dir)
-    csv_path = joinpath(repo_dir, "data", full_file_name)
-    println("File path in Github: ", csv_path)
+    url = "https://api.github.com/repos/$repo_owner/$repo_name/contents/$file_path"
 
+    headers = Dict(
+        "Authorization" => "Bearer $access_token",
+        "Accept" => "application/vnd.github.v3+json"
+    )
 
-    # Using the input data create a CSV file based on the input name
-    CSV.write(csv_path, input_data_source)
+    encoded_data = String(Base64.encode(UInt8(data)))
 
-    return csv_path
+    payload = Dict(
+        "message" => "Update CSV file",
+        "content" => encoded_data,
+        "branch" => branch_name
+    )
+
+    response = HTTP.request(
+        "PUT",
+        url,
+        headers,
+        JSON.json(payload)
+    )
+
+    return response
     
 end
 
 
 df_test_1 = DataFrame(package_name = ["AAindex"], package_uuid = ["1cd36ffe-cb05-4761-9ff9-f7bc1999e190"])
-csv_path = export_csv("output_test", df_test_1)  # Saves the DataFrame to "data/output.csv"
-
-# Commit and push the CSV file to the repository
-#run(`git add $csv_update`)
-#run(`git commit -m "Add CSV file"`)
-#run(`git push`)
-
-
-# Upload the CSV file to the GitHub repository using GitHub API
 repo_owner = "analyticsinmotion"
 repo_name = "julia-packages-data"
 branch_name = "main"
+file_path = "data/output_test.csv"
 TOKEN = ENV["TOKEN"]
 
-url = "https://api.github.com/repos/$repo_owner/$repo_name/contents/$csv_path"
-headers = Dict("Authorization" => "token $TOKEN")
+export_csv(df_test_1, repo_owner, repo_name, branch_name, file_path, TOKEN)
 
-# Read the CSV content
-csv_content = read(csv_path, String)
 
-# Create the request payload
-payload = Dict(
-    "message" => "Add CSV file",
-    "content" => base64encode(csv_content),
-    "branch" => branch_name
-)
 
-response = HTTP.request("PUT", url, headers=headers, json=payload)
+
 
 println("CSV file uploaded to the repository.")
 
